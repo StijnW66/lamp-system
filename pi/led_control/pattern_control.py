d0e6c08573c49ebe39aa1626beedeb8ecd73e1f7
StijnW66 <vandewaterstijn@gmail.com>
2021-08-29 12:16:53 +0100

    Allow for fully customizable patterns

--- pi/led_control/pattern_control.py
+++ pi/led_control/pattern_control.py
@@ -8,7 +8,15 @@ sys.path.append('.')
 
 from pi.led_control.led_control import set_rgb
 
-rainbow = [[100, 0, 0], [100, 100, 0], [0, 100, 0], [0, 100, 100], [0, 0, 100], [100, 0, 100]]
+#rainbow = [[100, 0, 0], [100, 100, 0], [0, 100, 0], [0, 100, 100], [0, 0, 100], [100, 0, 100]]
+rainbow = [[[100, 0, 0], 50], [[100, 100, 0], 50], [[0, 100, 0], 50], [[0, 100, 100], 50], [[0, 0, 100], 50], [[100, 0, 100], 50]]
+fast_rainbow = [[[100, 0, 0], 10], [[100, 100, 0], 10], [[0, 100, 0], 10], [[0, 100, 100], 10], [[0, 0, 100], 10], [[100, 0, 100], 10]]
+rgb_flash = [[[100, 0, 0], 10], [[100, 0, 0], 1], [[0, 100, 0], 10], [[0, 100, 0], 1], [[0, 0, 100], 10], [[0, 0, 100], 1]]
+rgb_quick_flash = [[[100, 0, 0], 5], [[100, 0, 0], 1], [[0, 100, 0], 5], [[0, 100, 0], 1], [[0, 0, 100], 5], [[0, 0, 100], 1]]
+rgb_super_quick_flash = [[[100, 0, 0], 2], [[100, 0, 0], 1], [[0, 100, 0], 2], [[0, 100, 0], 1], [[0, 0, 100], 2], [[0, 0, 100], 1]]
+
+
+pattern = rainbow + fast_rainbow + rgb_flash + rgb_quick_flash * 2 + rgb_super_quick_flash * 3
 
 class PatternThread:
 
@@ -16,10 +24,10 @@ class PatternThread:
 		self.thread = None
 		self.stop_flag = False
 
-	def start_pattern(self, pattern=0):
+	def start_pattern(self, pattern_num=0):
 		self.stop_flag = False
 
-		self.thread = threading.Thread(target=lambda: self.advance_pattern(pattern), daemon=True)
+		self.thread = threading.Thread(target=lambda: self.advance_pattern(pattern_num), daemon=True)
 		self.thread.start()
 		print("new thread started")
 
@@ -30,17 +38,23 @@ class PatternThread:
 			self.thread = None
 			print("new thread stopped")
 
-	def advance_pattern(self, pattern=0, interpolate_duration=50):
-		counter = 0
-		while not self.stop_flag:
-			index = math.floor(counter / interpolate_duration)
-			next_index = (index + 1) % len(rainbow)
-			progress = (counter % interpolate_duration) * (100/interpolate_duration)
+	def advance_pattern(self, pattern_num=0):
+		pattern_counter = 0
+		interpolation_counter = 0
+		interpolate_duration = rainbow[pattern_counter][1]
 
-			rgb = interpolate_color(rainbow[index], rainbow[next_index], progress)
+		while not self.stop_flag:
+			progress = interpolation_counter * (100/interpolate_duration)
+
+			rgb = interpolate_color(pattern[pattern_counter][0], pattern[(pattern_counter + 1) % len(pattern)][0], progress)
+			print(rgb)
 			set_rgb(rgb)
 
-			counter = (counter + 1) % (interpolate_duration *len(rainbow))
+
+			interpolation_counter = (interpolation_counter + 1) % interpolate_duration
+			if interpolation_counter == 0:
+				pattern_counter = (pattern_counter + 1) % len(pattern)
+				interpolate_duration = pattern[pattern_counter][1]
 			sleep(0.1)
 
 
@@ -48,4 +62,5 @@ def interpolate_color(rgb_start, rgb_target, progress):
 	rgb_start = [x * ((100 - progress)/100) for x in rgb_start]
 	rgb_target = [x * (progress/100) for x in rgb_target]
 	rgb = list( map(add, rgb_start, rgb_target))
+	rgb = [int(x) for x in rgb]
 	return rgb
